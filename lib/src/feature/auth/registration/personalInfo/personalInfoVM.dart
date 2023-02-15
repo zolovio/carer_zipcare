97e7d2d410d1fc57e4fd7498429533e48dcd554d
Mohammad Rijwan <ridz.creation4@gmail.com>
2023-02-15 10:42:55 +0530

    feat():added personal info screen

---
+++ lib/src/feature/auth/registration/personalInfo/personalInfoVM.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zip_care/src/feature/auth/registration/qualification/qualificationScreen.dart';
+import 'package:zip_care/src/feature/auth/registration/searchAddress/searchAddressScreen.dart';
+
+final personalInfoVmProvider =
+    ChangeNotifierProvider.autoDispose<PersonalInfoVm>((ref) {
+  return PersonalInfoVm();
+});
+
+class PersonalInfoVm extends ChangeNotifier {
+  late List<TextInputFormatter> emojiHide;
+  late bool isPasswordHide = true;
+  bool isRemember = false;
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  TextEditingController phoneNo = TextEditingController();
+  TextEditingController address = TextEditingController();
+  TextEditingController postcode = TextEditingController();
+  TextEditingController streetAddress = TextEditingController();
+  TextEditingController streetAddress2 = TextEditingController();
+  TextEditingController city = TextEditingController();
+  TextEditingController country = TextEditingController();
+  TextEditingController dob = TextEditingController();
+  TextEditingController nationality = TextEditingController();
+  TextEditingController issueDate = TextEditingController();
+  TextEditingController certiNo = TextEditingController();
+  TextEditingController insuranceNo = TextEditingController();
+  String? certi;
+  String? isService;
+  File? file;
+
+  setCerti(String certi) {
+    certi = certi;
+    notifyListeners();
+  }
+
+  setService(String service) {
+    isService = service;
+    notifyListeners();
+  }
+
+  pickCertificate() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      file = File(result.files.single.path!);
+    }
+    notifyListeners();
+  }
+
+  onNext(context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => QualificationScreen()));
+  }
+}
