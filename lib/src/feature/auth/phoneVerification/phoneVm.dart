8707159e7a23b85444088cb975bff229eddae5b1
Mohammad Rijwan <ridz.creation4@gmail.com>
2023-02-15 10:35:51 +0530

    feat():added phone verification

---
+++ lib/src/feature/auth/phoneVerification/phoneVm.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zip_care/src/feature/auth/emailVerification/emailVerificationScreen.dart';
+
+final phoneVerificationVmProvider =
+    ChangeNotifierProvider.autoDispose<PhoneVerificationVm>((ref) {
+  return PhoneVerificationVm();
+});
+
+class PhoneVerificationVm extends ChangeNotifier {
+  late TextEditingController emailController;
+  late List<TextInputFormatter> emojiHide;
+
+  PhoneVerificationVm() {
+    onInit();
+  }
+
+  void onInit() {
+    intializeEditText();
+  }
+
+  //!*===================================== intialize all controller that are used in controller ====================================*/
+
+  intializeEditText() {
+    disableEmoji();
+    emailController = TextEditingController();
+  }
+
+  disableEmoji() {
+    emojiHide = [
+      FilteringTextInputFormatter(
+          RegExp(
+              r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])'),
+          allow: false)
+    ];
+  }
+
+  void onVerify(context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => EmailVerificationScreen()));
+    notifyListeners();
+  }
+}
