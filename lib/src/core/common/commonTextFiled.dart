add41bdba70c910cbb35ec7933b5a823d41f53a6
Mohammad Rijwan <ridz.creation4@gmail.com>
2023-02-15 10:36:20 +0530

    feat():common text field

---
+++ lib/src/core/common/commonTextFiled.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:zip_care/src/core/constants/colors.dart';
+import 'package:zip_care/src/core/constants/dimens.dart';
+import 'package:zip_care/src/core/constants/font_family.dart';
+
+class CommonTextFiled extends StatelessWidget {
+  final String name;
+  final String hintText;
+  final TextEditingController controller;
+  final Widget suffixIcon;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final TextInputAction textInputAction;
+  final List<TextInputFormatter>? inputFormatters;
+  final bool isEnabled;
+
+  const CommonTextFiled(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.name,
+      required this.validator,
+      this.suffixIcon = const SizedBox(),
+      this.obscureText = false,
+      this.keyboardType = TextInputType.emailAddress,
+      this.textInputAction = TextInputAction.next,
+      this.inputFormatters,
+      this.isEnabled = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilderTextField(
+        enabled: isEnabled,
+        controller: controller,
+        obscureText: obscureText,
+        inputFormatters: inputFormatters,
+        decoration: InputDecoration(
+            suffixIcon: suffixIcon,
+            hintText: hintText,
+            hintStyle: const TextStyle(fontFamily: FontFamily.lexendRegular),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius_15),
+                borderSide:
+                    const BorderSide(color: AppColors.lightestGreyColor)),
+            errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius_15)),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius_15),
+                borderSide:
+                    const BorderSide(color: AppColors.lightestGreyColor)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius_15),
+                borderSide: const BorderSide(color: AppColors.darkPinkColor)),
+            focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(radius_15),
+                borderSide: const BorderSide(color: AppColors.colorPink))),
+        validator: validator,
+        keyboardType: keyboardType,
+        textInputAction: textInputAction,
+        name: name);
+  }
+}
